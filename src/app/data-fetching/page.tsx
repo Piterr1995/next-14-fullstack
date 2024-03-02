cb0611d6e35c8fae9a6d0ccb191d2a606b342773
Piotr Zawierucha <piotr.zawierucha@ing.com>
2024-03-02 21:27:29 +0100

    comment data fetching page for better build

--- src/app/data-fetching/page.tsx
+++ src/app/data-fetching/page.tsx
@@ -1,5 +1,6 @@
 export default async function DataFetchingPage() {
-  const res = await fetch("http://localhost:3000/api/items");
-  const data = await res.json();
-  return <div>{JSON.stringify(data)}</div>;
+  // const res = await fetch("http://localhost:3000/api/items");
+  // const data = await res.json();
+  // return <div>{JSON.stringify(data)}</div>;
+  return <div>Hello</div>;
 }
